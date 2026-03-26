ad3656038fd8e69248d1495a4ff1d4708adab06a
CodemeMax3 <codesob30@email.com>
2026-03-26 14:06:01 -0400

    Completed logout, login and templates fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,65 +1,82 @@
-# Uncomment the required imports before adding the code
-
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
-
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from datetime import datetime
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 
-
-# Get an instance of a logger
+# Logger
 logger = logging.getLogger(__name__)
 
-
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
+# ------------------ LOGIN ------------------
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+
     user = authenticate(username=username, password=password)
     data = {"userName": username}
+
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
         data = {"userName": username, "status": "Authenticated"}
+
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+# ------------------ LOGOUT ------------------
+@csrf_exempt
+def logout_request(request):
+    logout(request)
+    data = {"userName": ""}
+    return JsonResponse(data)
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+# ------------------ REGISTRATION ------------------
+@csrf_exempt
+def registration(request):
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data.get('firstName', '')
+    last_name = data.get('lastName', '')
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    try:
+        user = User.objects.get(username=username)
+        return JsonResponse({"userName": username, "error": "Already Exists"})
+    except:
+        user = User.objects.create_user(
+            username=username,
+            password=password,
+            first_name=first_name,
+            last_name=last_name
+        )
+        user.save()
+        return JsonResponse({"userName": username})
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+
+# ------------------ HOME / DEALERS ------------------
+def get_dealerships(request):
+    return render(request, "Home.html")
+
+
+# ------------------ DEALER REVIEWS ------------------
+def get_dealer_reviews(request, dealer_id):
+    return render(request, "DealerDetails.html", {"dealer_id": dealer_id})
+
+
+# ------------------ DEALER DETAILS ------------------
+def get_dealer_details(request, dealer_id):
+    return render(request, "DealerDetails.html", {"dealer_id": dealer_id})
+
+
+# ------------------ ADD REVIEW ------------------
+@csrf_exempt
+def add_review(request):
+    if request.method == "POST":
+        return JsonResponse({"status": "Review Added"})
